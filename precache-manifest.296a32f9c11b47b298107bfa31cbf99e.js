1cbf9e6ec4db5815cc33ccfe03fd13f052c3c705
Zigy <zigybass@gmail.com>
2019-09-23 21:29:25 -0400

    Updates

---
+++ precache-manifest.296a32f9c11b47b298107bfa31cbf99e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02c1ff46f537af630b32cb95e02d7c48",
+    "url": "/React-Memory-Game/index.html"
+  },
+  {
+    "revision": "ec76c2484cb8d0d15287",
+    "url": "/React-Memory-Game/static/js/2.3fa1ee7b.chunk.js"
+  },
+  {
+    "revision": "14bee9b58d0345a695b6",
+    "url": "/React-Memory-Game/static/js/main.d8a076b1.chunk.js"
+  },
+  {
+    "revision": "7f27d1e07012921ecad4",
+    "url": "/React-Memory-Game/static/js/runtime-main.25dee0c9.js"
+  },
+  {
+    "revision": "00722a1a1081b3eaafacecd163e41768",
+    "url": "/React-Memory-Game/static/media/bergen.00722a1a.jpg"
+  },
+  {
+    "revision": "fd309de4d733dbc7656fc4e748564044",
+    "url": "/React-Memory-Game/static/media/bern.fd309de4.jpg"
+  },
+  {
+    "revision": "a080b39fe5ee2c946a035b000f999731",
+    "url": "/React-Memory-Game/static/media/bonn.a080b39f.jpg"
+  },
+  {
+    "revision": "697391c28f64049b8d727b1956fcadab",
+    "url": "/React-Memory-Game/static/media/greece.697391c2.jpg"
+  },
+  {
+    "revision": "64dd3a7cb51052ab6a52cb2ad3bc9494",
+    "url": "/React-Memory-Game/static/media/kyoto.64dd3a7c.jpg"
+  },
+  {
+    "revision": "7945ac2d47e8f4cdf8ff348c70d9f229",
+    "url": "/React-Memory-Game/static/media/marrakech.7945ac2d.jpg"
+  },
+  {
+    "revision": "e959c7d3025f716426ce4d80e6206bd2",
+    "url": "/React-Memory-Game/static/media/newyork.e959c7d3.jpg"
+  },
+  {
+    "revision": "9f8a31cdf80acd27b64652d682823dcb",
+    "url": "/React-Memory-Game/static/media/oregon.9f8a31cd.jpg"
+  },
+  {
+    "revision": "e0a328bdab91613b8abe0a1e65fe882f",
+    "url": "/React-Memory-Game/static/media/rome.e0a328bd.jpg"
+  },
+  {
+    "revision": "05bc90cf422713f450adecd24044c44b",
+    "url": "/React-Memory-Game/static/media/russia.05bc90cf.jpg"
+  },
+  {
+    "revision": "c62298c3730c5ec31c679f7bc88bce8e",
+    "url": "/React-Memory-Game/static/media/seoul.c62298c3.jpg"
+  },
+  {
+    "revision": "781ca864360a850aeb53a15f00e74bd1",
+    "url": "/React-Memory-Game/static/media/tibet.781ca864.jpg"
+  }
+]);
