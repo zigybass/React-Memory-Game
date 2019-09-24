45338cf29562b91f37ff5d86738630c8df603fe7
Zigy <zigybass@gmail.com>
2019-09-23 21:18:16 -0400

    serve installed and added pbuild path

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const path = require('path');
+const app = express();
 
 const app = express();
 const PORT = process.argv.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'build')));
 
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
@@ -12,7 +16,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(PORT, function() {
   console.log("App listening on Port: " + PORT);
